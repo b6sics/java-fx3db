20952da967d9a6a955353288799862d5144639e6
kiss j gabi <b6@b6.hu>
2019-11-08 22:48:32 +0100

    add 01B - létrehozás java-fx project

---
+++ 01BInsertInto/src/workers/DB.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workers;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author g
+ */
+public class DB {
+
+    final String dbUrl1 = "jdbc:mysql://localhost:3306/";
+    final String dbUrl2 = "jdbc:mysql://localhost:3306/dolgozok"
+            + "?useUnicode=true&characterEncoding=UTF-8";
+    final String user = "root";
+    final String pass = "";
+
+    public DB() {
+        String s1 = "CREATE DATABASE IF NOT EXISTS dolgozok "
+                + "DEFAULT CHARSET=utf8mb4 "
+                + "COLLATE=utf8mb4_hungarian_ci;";
+        String s2 = "USE dolgozok;";
+        String s3 = "CREATE TABLE IF NOT EXISTS adatok ("
+                + "id int(4) NOT NULL AUTO_INCREMENT,"
+                + "nev varchar(50),"
+                + "szulido date,"
+                + "fizetes int(7),"
+                + "PRIMARY KEY(id)"
+                + ");";
+        try (Connection kapcs = (Connection) DriverManager.getConnection(dbUrl1, user, pass);
+                PreparedStatement ekp1 = (PreparedStatement) kapcs.prepareStatement(s1);
+                PreparedStatement ekp2 = (PreparedStatement) kapcs.prepareStatement(s2);
+                PreparedStatement ekp3 = (PreparedStatement) kapcs.prepareStatement(s3)) {
+            ekp1.execute();
+            ekp2.execute();
+            ekp3.execute();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void lista() {
+        String s = "SELECT * FROM adatok";
+        try (Connection kapcs = (Connection) DriverManager.getConnection(dbUrl2, user, pass);
+                PreparedStatement ekp = (PreparedStatement) kapcs.prepareStatement(s)) {
+            ResultSet eredmeny = ekp.executeQuery();
+            while (eredmeny.next()) {
+                System.out.printf("%2d %-50s %s %10d\n",
+                        eredmeny.getInt("id"),
+                        eredmeny.getString("nev"),
+                        eredmeny.getString("szulido"),
+                        eredmeny.getInt("fizetes"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void uj(String nev, String szulido, int fizetes) {
+        String s = "INSERT INTO adatok (nev, szulido, fizetes) "
+                + "VALUES (?, ?, ?);";
+        try (Connection kapcs = (Connection) DriverManager.getConnection(dbUrl2, user, pass);
+                PreparedStatement ekp = (PreparedStatement) kapcs.prepareStatement(s)) {
+            ekp.setString(1, nev);
+            ekp.setString(2, szulido);
+            ekp.setInt(3, fizetes);
+            ekp.executeUpdate();
+            System.out.println(nev + " hozzáadva.");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void beolvas(String fnev) {
+        try (Scanner be = new Scanner(new File(fnev))) {
+            while (be.hasNextLine()) {
+                String[] sor = be.nextLine().split(",");
+                uj(sor[0], sor[1], Integer.parseInt(sor[2]));
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void kiir(String fnev) {
+        try (PrintWriter ki = new PrintWriter(fnev)) {
+            try (Connection kapcs = (Connection) DriverManager.getConnection(dbUrl2, user, pass);
+                    PreparedStatement ekp = (PreparedStatement) kapcs.prepareStatement("SELECT * FROM adatok;")) {
+                ResultSet eredmeny = ekp.executeQuery();
+                while (eredmeny.next()) {
+                    ki.println(eredmeny.getString("nev") + ","
+                            + eredmeny.getString("szulido") + ","
+                            + eredmeny.getInt("fizetes"));
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void emel(int szazalek) {
+        String s = "UPDATE adatok SET fizetes = (1+?/100)*fizetes;";
+        try (Connection kapcs = (Connection) DriverManager.getConnection(dbUrl2, user, pass);
+                PreparedStatement ekp = (PreparedStatement) kapcs.prepareStatement(s)) {
+            ekp.setInt(1, szazalek);
+            int sorok = ekp.executeUpdate();
+            System.out.println(sorok + " sor módosítva.");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void torol() {
+        String s = "DELETE FROM adatok;";
+        try (Connection kapcs = (Connection) DriverManager.getConnection(dbUrl2, user, pass);
+                PreparedStatement ekp = (PreparedStatement) kapcs.prepareStatement(s)) {
+            int sorok = ekp.executeUpdate();
+            System.out.println(sorok + " sor törölve");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ 01BInsertInto/src/workers/Workers.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workers;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author g
+ */
+public class Workers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DB ab = new DB();
+        Scanner bill = new Scanner(System.in, "cp1250");
+        boolean tovabb = true;
+        String nev, szulido, fnev;
+        int fizetes;
+
+        while (tovabb) {
+            System.out.println("1-listázás 2-új 3-importálás 4-exportálás "
+                    + "5-emelés 6-törlés 0-kilépés");
+            System.out.print("Válasz: ");
+            String v = bill.nextLine();
+            switch (v.charAt(0)) {
+                case '1':
+                    ab.lista();
+                    break;
+                case '2':
+                    System.out.print("Név: ");
+                    nev = bill.nextLine();
+                    System.out.print("Születési idő: ");
+                    szulido = bill.nextLine();
+                    System.out.print("Fizetés: ");
+                    fizetes = bill.nextInt();
+                    bill.nextLine();
+                    ab.uj(nev, szulido, fizetes);
+                    break;
+                case '3':
+                    System.out.print("Fájlnév: ");
+                    fnev = bill.nextLine();
+                    ab.beolvas(fnev);
+                    break;
+                case '4':
+                    System.out.print("Fájlnév: ");
+                    fnev = bill.nextLine();
+                    ab.kiir(fnev);
+                    break;
+                case '5':
+                    System.out.print("Százalék: ");
+                    int sz = bill.nextInt();
+                    bill.nextLine();
+                    ab.emel(sz);
+                    break;
+                case '6':
+                    System.out.print("Törölni szeretnél minden dolgozót?(i/n): ");
+                    char igen = bill.nextLine().charAt(0);
+                    if (igen == 'i') {
+                        ab.torol();
+                    }
+                    break;
+                default:
+                    tovabb = false;
+            }
+        }
+    }
+}
